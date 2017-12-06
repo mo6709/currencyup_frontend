bb87e2ea9c1dd0f900dd9160efc7a0129a148ad9
Moshe Levi <mo6709@gmail.com>
2017-12-06 18:06:58 -0500

    update package.json

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/index';
+
+const store = createStore(
+    rootReducer,
+    compose(
+        applyMiddleware(thunk),
+        window.devToolsExtension ? window.devToolsExtension() : f => f
+    )
+)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
