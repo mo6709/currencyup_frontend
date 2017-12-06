b36fc12e0c4c0e08726e6f6afcced93fca342311
Moshe Levi <mo6709@gmail.com>
2017-12-06 16:26:39 -0500

    add currencies reducer

---
+++ src/reducers/investmentsReducer.js
@@ -0,0 +1,12 @@
+export default function investmentsReducer(state = {
+    loading: false, all = []
+}, action){
+    switch (action.type) {
+        case 'LOADING_INVESTMENTS':  
+            return Object({}, state, { loading: true });
+        case 'FETCH_INVESTMENTS':    
+            return Object({}, state, { loading: false, all: action.payload });
+        default:
+            return state;
+    }
+}
