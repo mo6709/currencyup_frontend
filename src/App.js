9e812d605f6309386786911d897ade288b03a98a
Moshe Levi <mo6709@gmail.com>
2017-12-07 16:12:53 -0500

    add currencies route to app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, NavLink, Route } from 'react-router-dom';
+import CurrenciesPage from './containers/currencies/CurrenciesPage';
 
 class App extends Component {
   render() {
@@ -21,6 +22,7 @@ class App extends Component {
               <NavLink style={{ marginRight: '10px' }} to="/">Login</NavLink> 
             </div>
             <Route exact path="/" render={() => <h3>Welcome to currencyUP</h3>} />
+            <Route exact path="/currencies" component={CurrenciesPage} />
           </div>  
         </Router>  
       </div>
