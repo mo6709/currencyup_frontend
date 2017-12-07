6d17441292e810cf3e0c67304de7673152b8dd58
Moshe Levi <mo6709@gmail.com>
2017-12-07 16:37:32 -0500

    remove actions from account reduder

--- src/reducers/accountReducer.js
+++ src/reducers/accountReducer.js
@@ -1,13 +1,7 @@
 export default function accountReducer(state = {
     loading: false, account: {}
 }, action){
-    switch (action.type) {
-        case 'ACCOUNT_SIGNUP':
-            return Object.assign({}, state, { account: action.payload });
-        case 'ACCOUNT_SIGNIN':
-            return Object.assign({}, state, {});
-        case 'ACCOUNT_LOGOUT':
-            return Object.assign({}, state, {});       
+    switch (action.type) {     
         case 'ACCOUNT_UPDATE':
             return Object.assign({}, state, {});        
         default:
