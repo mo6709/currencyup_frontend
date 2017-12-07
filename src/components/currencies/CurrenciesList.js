68199654ea4a5e72c1003b26853a62d046c2dc9b
Moshe Levi <mo6709@gmail.com>
2017-12-07 16:15:14 -0500

    add CurrenciesList file to component

---
+++ src/components/currencies/CurrenciesList.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const CurrencyList = ({ CurrenciesData }) => {
+    const currencyDivs = CurrenciesData.map(currency => {
+        return (
+            <div key={currency.id} className="currency">
+              <h2>Name: {currency.name}</h2>
+              <h4>Rate: {currency.rate}</h4>
+              <h4>Region: {currency.region}</h4>
+            </div>
+        )
+    })
+
+    return (
+        <div>
+            <h1>All Currencies</h1>
+            {currencyDivs}
+        </div>    
+    )
+}
+
+export default CurrencyList;
