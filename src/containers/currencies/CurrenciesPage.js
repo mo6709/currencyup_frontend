8851233c715ff4f5a4eaf331003f9f5acfa927f4
Moshe Levi <mo6709@gmail.com>
2017-12-07 16:14:10 -0500

    connect currenciesPage with redux functions

--- src/containers/currencies/CurrenciesPage.js
+++ src/containers/currencies/CurrenciesPage.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 import * as currencyActions from '../../actions/currencyActions';
-import CurrenciesList from '../../components/currencies/CurrenciesList'
+import CurrenciesList from '../../components/currencies/CurrenciesList';
+import { bindActionCreators } from 'redux';
 
 class CurrenciesPage extends Component {
     constructor(props){
@@ -11,11 +12,32 @@ class CurrenciesPage extends Component {
         this.state = {}
     }
 
+    componentDidMount() {
+        if(this.props.currencies.length === 0){
+            this.props.currencyActions.fetchCurrencies()
+        }
+    }
+
     render(){
         return(
-           <h1>hello from CurrenciesPage smart container</h1> 
+           <div>
+             <h1>hello from CurrenciesPage smart container</h1>
+             <CurrenciesList CurrenciesData={this.props.currencies}/> 
+           </div>
         )
     }
 }
 
-export default CurrenciesPage;
+const mapStateToProps = (state) => {
+    return {
+        currencies: state.currencies.all
+    }
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        currencyActions: bindActionCreators(currencyActions, dispatch)
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CurrenciesPage);
