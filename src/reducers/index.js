61c3b6b08aafb0c37899609dbf0a09b3945a39b8
Moshe Levi <mo6709@gmail.com>
2017-12-11 16:08:30 -0500

    add sessionReducer to index in reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,14 @@ import { combineReducers } from 'redux'
 import accountReducer from './accountReducer'
 import currenciesReducer from './currenciesReducer'
 import investmentsReducer from './investmentsReducer'
+import sessionReducer from './sessionReducer'
 
 const rootReducer = combineReducers({
     account: accountReducer,
     currencies: currenciesReducer,
-    investments: investmentsReducer
+    investments: investmentsReducer,
+    session: sessionReducer
+
 })
 
 export default rootReducer;
