0e584f22583a6f9be43ca37dcdb231d4b04b23ec
Moshe Levi <mo6709@gmail.com>
2017-12-18 18:54:11 -0500

    edit accountActions but NOT in use yet

--- src/actions/accountActions.js
+++ src/actions/accountActions.js
@@ -1,17 +1,29 @@
 import fetch from 'isomorphic-fetch'
 
-export function accountSignup(){
-
+export function getAccountInfo(accountType){
+    return function(dispatch)
+        debugger;
+        const account_uri = `api/v1/${accountType}s/${localStorage.account_id}`
+        fetch(account_uri, {
+            method: 'GET',
+            headers: { 'AUTHORIZATION': `${localStorage.token}`, 'Content-Type': 'application/json' }
+            }
+        )
+        .then(response => response.json())
+        .then(responseJSON => {
+            dispatch(
+                { 
+                    type: "SET_ACCOUNT", 
+                    payload: { 
+                        accountType: credentials["accountType"], 
+                        info: responseJSON 
+                    } 
+                }
+           )
+        })
+   }
 }
 
-export function accountSignin(){
-    
-}
-
-export function accountLogout(){
-    
-}
-
-export function accountUpdate(){
-    
+function setAccount(accountInfo) {
+    return {type: "SET_ACCOUNT", payload: accountInfo.data} 
 }
