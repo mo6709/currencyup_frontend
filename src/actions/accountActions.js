f2249469c4334792a3febb8c6fad01a4fa5674ef
Moshe Levi <mo6709@gmail.com>
2017-12-20 10:27:58 -0500

    fix getAndSetAccountInfo(), signupAccount(), setAccount() functions in accountActios

--- src/actions/accountActions.js
+++ src/actions/accountActions.js
@@ -1,29 +1,42 @@
 import fetch from 'isomorphic-fetch'
 
-export function getAccountInfo(accountType){
-    return function(dispatch)
-        debugger;
-        const account_uri = `api/v1/${accountType}s/${localStorage.account_id}`
-        fetch(account_uri, {
-            method: 'GET',
-            headers: { 'AUTHORIZATION': `${localStorage.token}`, 'Content-Type': 'application/json' }
-            }
-        )
-        .then(response => response.json())
-        .then(responseJSON => {
-            dispatch(
-                { 
-                    type: "SET_ACCOUNT", 
-                    payload: { 
-                        accountType: credentials["accountType"], 
-                        info: responseJSON 
-                    } 
-                }
-           )
-        })
-   }
+export function getAndSetAccountInfo(dispatchAction, type){
+    const account_uri = `api/v1/${type}s/${localStorage.account_id}`;
+    fetch(account_uri, {
+        method: 'GET',
+        headers: { 'AUTHORIZATION': `${localStorage.token}`, 'Content-Type': 'application/json' }
+    })
+    .then(response => response.json())
+    .then(responseJSON => {
+        dispatchAction(setAccount(type, responseJSON))
+    })           
 }
 
-function setAccount(accountInfo) {
-    return {type: "SET_ACCOUNT", payload: accountInfo.data} 
+export function signupAccount(credentials){
+    const { email, password, accountType } = credentials;
+    return function(dispatch){
+        const dispatcher = dispatch
+        const paramters =  { corporation: { email: email, password: password } } 
+        const uri = `api/v1/${accountType}_signup`
+        return fetch(uri, { 
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(paramters) 
+        })
+        .then(response => response.json())
+        .then(responseJSON => {
+            localStorage.setItem('token', responseJSON.token);
+            localStorage.setItem('account_id', responseJSON.account_id);
+            dispatcher({ type: "LOGIN_SUCCESS" });
+            getAndSetAccountInfo(dispatcher, accountType);
+        }).catch(error => { throw(error) })
+    }
+}
+
+function setAccount(type, response) {
+    return { type: "SET_ACCOUNT", payload: { 
+            accountType: type, 
+            info: response 
+        }
+    } 
 }
