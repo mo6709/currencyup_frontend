5944d75580c5fb9d06cdde0cdb1a0731d9693f1e
Moshe Levi <mo6709@gmail.com>
2017-12-20 10:25:45 -0500

    add signup feature

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import { BrowserRouter as Router, NavLink, Route } from 'react-router-dom';
 import CurrenciesPage from './containers/currencies/CurrenciesPage';
 import LoginForm from './containers/session/LoginForm'
+import SignupForm from './containers/account/SignupForm'
 
 class App extends Component {
   render() {
@@ -21,10 +22,12 @@ class App extends Component {
             <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
               <NavLink style={{ marginRight: '10px' }} to="/currencies">See All The Currencies!</NavLink>
               <NavLink style={{ marginRight: '10px' }} activeStyle={{ background: 'darkblue' }} exact to="/login">Login</NavLink> 
+              <NavLink style={{ marginRight: '10px' }} activeStyle={{ background: 'darkblue' }} exact to="/signup">Signup</NavLink> 
             </div>
             <Route exact path="/" render={() => <h3>Welcome to currencyUP</h3>} />
             <Route exact path="/currencies" component={CurrenciesPage} />
             <Route exact path="/login" component={LoginForm} />
+            <Route exact path="/signup" component={SignupForm} />
           </div>  
         </Router>  
       </div>
