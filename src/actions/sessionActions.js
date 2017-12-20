c1c17f22a0ba5a6733678dc9c717f1febcd4e677
Moshe Levi <mo6709@gmail.com>
2017-12-20 10:30:52 -0500

    fix loginAccount() function in sessionActions

--- src/actions/sessionActions.js
+++ src/actions/sessionActions.js
@@ -1,13 +1,11 @@
 import fetch from 'isomorphic-fetch';
-// import { getAccountInfo } from './accountActions'
-
-export function signupAccount(){
-    
-}
+import { getAndSetAccountInfo } from './accountActions'
     
 export function loginAccount(credentials){
+    const { accountType } = credentials;
     return function(dispatch){
-        const uri = `api/v1/${credentials["accountType"]}_login`
+        const dispatcher = dispatch
+        const uri = `api/v1/${accountType}_login`
         return fetch(uri, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -17,39 +15,11 @@ export function loginAccount(credentials){
         .then(responseJSON => {    
             localStorage.setItem('token', responseJSON.token);
             localStorage.setItem('account_id', responseJSON.account_id);
-            
-            const account_uri = `api/v1/${credentials["accountType"]}s/${localStorage.account_id}`
-            fetch(account_uri, {
-                method: 'GET',
-                headers: { 'AUTHORIZATION': `${localStorage.token}`, 'Content-Type': 'application/json'}
-                }
-            )
-            .then(response => response.json())
-            .then(responseJSON => {
-                dispatch(
-                    {   
-                        type: "SET_ACCOUNT", 
-                        payload: { 
-                            accountType: credentials["accountType"], 
-                            info: responseJSON 
-                        } 
-                    }
-                )
-                dispatch(loginSuccess(responseJSON));
-            })
-            .catch(error => {
-                throw(error)
-            })
-            
+            dispatcher({ type: "LOGIN_SUCCESS" })
+            getAndSetAccountInfo(dispatcher, accountType)
         })
-        .catch(error => {
-            throw(error);
-        })
+        .catch(error => { throw(error) })
     }
-}
-
-export function loginSuccess(token){
-    return { type: "LOGIN_SUCCESS", payload: token }
 }
 
 export function logoutAccount(){
