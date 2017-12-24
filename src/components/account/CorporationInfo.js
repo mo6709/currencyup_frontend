064ae70434ab0279103fe68f05f5b9ad8fca1ee0
Moshe Levi <mo6709@gmail.com>
2017-12-24 16:48:30 -0500

    edit CorporationInfo in components

--- src/components/account/CorporationInfo.js
+++ src/components/account/CorporationInfo.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const CorporationInfo = ({ accountInfo }) => {
-    const { id, name } = accountInfo.info;
+    const { id, name, title } = accountInfo.info;
     return(
         <div>
             <p>Hello form CorporationInfo smart container</p>
-            <p>Id: {id}</p><h1>Corporation Name:{name}</h1>
+            <p>Id: {id}</p>
+            <h1>Corporation Name:{name}</h1>
+            <h2>Title: {title}</h2>
         </div>
     )
 }
