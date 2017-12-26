5f0d78174174182df0fc833fdfcfeb8afd9473ac
Moshe Levi <mo6709@gmail.com>
2017-12-26 14:24:04 -0500

    edit AccountEditForm.js

--- src/containers/account/AccountEditForm.js
+++ src/containers/account/AccountEditForm.js
@@ -1,31 +1,62 @@
+import 'react-select/dist/react-select.css';
+
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { FormGroup, ControlLabel, FormControl} from 'react-bootstrap'
+import Select from 'react-select';
+
+const divStyle = {
+  margin: '40px',
+  border: '5px '
+};
 
 class AccountEditForm extends Component{
     constructor(props){
         super(props);
 
-        this.state = {}
+        this.state = {
+            select: {
+                removeSelected: true,
+                disabled: false,
+                stayOpen: false,
+                value: [],
+                rtl: false,
+            }      
+        }
     }
 
-    componentDidMount(){
-        this.setState({
-            ...this.props.account
-        })
+
+    componentWillMount(){
+        const newState = Object.assign({}, this.state);
+        newState.account = this.props.account;
+        newState.select.value = this.props.account.regions_array
+        this.setState(newState);
     }
 
     handelInputChange = (event) => {
-        const { name, value } = event.target;
-        this.setState({ [name]: value })
-        debugger;
+        const { name ,value } =  event.target;
+        const parstValue = name === "investment_period" ? parseInt(value) : value
+        this.setState({ account: Object.assign({}, this.state.account, { [name]: parstValue }) })
+    }
+
+    handleSelectChange = (value) => {        
+        const newState = Object.assign({}, this.state);
+        newState.select.value = value.split(",") 
+        this.setState(newState);
     }
 
     handelEditSubmit = (event) => {
-        
+        debugger;
     }
 
-    render(){ 
-        return(
+
+    render(){
+        const { disabled, stayOpen, value } = this.state.select;
+        let regions = this.props.currencies.map((currency) => {
+            return { label: currency.region, value: currency.region }
+        });
+
+        return( 
             <div>
                 <p>Hello from AccountEdit smart Container</p>
                  
@@ -34,7 +65,7 @@ class AccountEditForm extends Component{
                         <input type="email"
                             name="email"
                             placeholder="Enter email"
-                            value={this.state.email}
+                            value={this.state.account.email}
                             onChange={this.handelInputChange}/>
                     </label><br/>
 
@@ -42,7 +73,7 @@ class AccountEditForm extends Component{
                         <input type="text"
                             name="name"
                             placeholder="Enter name"
-                            value={this.state.name}
+                            value={this.state.account.name}
                             onChange={this.handelInputChange}/>
                     </label><br/>
 
@@ -50,27 +81,32 @@ class AccountEditForm extends Component{
                         <input type="text"
                             name="title"
                             placeholder="Enter Title"
-                            value={this.state.title}
+                            value={this.state.account.title}
                             onChange={this.handelInputChange}/>
                     </label><br/>
 
-                    <label>
-                        Select investment regions:
-                        <select name="regions_array" multiple={true} value={this.state.regions_array} onChange={this.handleChange}>
-                            <option value="Israel">Israel</option>
-                            <option value="Jepan">Jepan</option>
-                            <option value="USA">USA</option>
-                        </select>
-                    </label><br/>
-
-
+                  <div style={divStyle}>
+                    Select Regions:
+                    <Select
+                        closeOnSelect={!stayOpen}
+                        disabled={disabled}
+                        multi
+                        onChange={this.handleSelectChange}
+                        options={regions}
+                        placeholder="Select investment regions"
+                        removeSelected={this.state.select.removeSelected}
+                        rtl={this.state.select.rtl}
+                        simpleValue
+                        value={value}/>
+                  </div>
+                   
 
                     <div>
                         <label>6 months period
                             <input type="radio"
                                 name="investment_period"
                                 value="6" 
-                                checked={this.state.investment_period === "6"}
+                                checked={this.state.account.investment_period === 6 }
                                 onChange={this.handelInputChange}/>
                         </label><br/>
                         
@@ -78,7 +114,7 @@ class AccountEditForm extends Component{
                             <input type="radio"
                                 name="investment_period"
                                 value="3" 
-                                checked={this.state.investment_period === "3"}
+                                checked={this.state.account.investment_period === 3 }
                                 onChange={this.handelInputChange}/>
                         </label><br/>
                         
@@ -86,7 +122,7 @@ class AccountEditForm extends Component{
                             <input type="radio"
                                 name="investment_period" 
                                 value="2"
-                                checked={this.state.investment_period === "2"}
+                                checked={this.state.account.investment_period === 2 }
                                 onChange={this.handelInputChange}/>
                         </label><br/>
                     </div>
@@ -99,7 +135,10 @@ class AccountEditForm extends Component{
 }
 
 const MapStateToProps = (state) => {
-    return { account: state.account.info }
+    return { 
+        account: state.account.info, 
+        currencies: state.currencies.all 
+    }
 }
 
 
