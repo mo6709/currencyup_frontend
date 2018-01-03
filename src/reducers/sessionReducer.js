f357b146bdda5c073f6d10b9762b500a5f512d7e
Moshe Levi <mo6709@gmail.com>
2018-01-03 11:47:19 -0500

    edit sessionReducer

--- src/reducers/sessionReducer.js
+++ src/reducers/sessionReducer.js
@@ -1,15 +1,15 @@
 export default function sessionReducer(state = {
      loggedIn: !!localStorage.getItem('token'),
-     error: '',
+     errors: '',
      loading: false
     }, action){
     switch (action.type) {
-        case 'LOADING':
+        case 'LOGIN_LOADING':
             return Object.assign({}, state, { loading: true }) ;
         case 'LOGIN_FAILUR':
-            return { loggedIn: false, error: action.message, loading: false }
+            return { loggedIn: false, errors: action.message, loading: false }
         case 'LOGIN_SUCCESS': 
-            return { loggedIn: !!localStorage.getItem('token'), error: "", loading: false }   
+            return { loggedIn: !!localStorage.getItem('token'), errors: "", loading: false }   
         case 'LOGOUT_SUCCESS':
             return ;  
         default:
