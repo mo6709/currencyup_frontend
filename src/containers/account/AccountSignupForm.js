077ceab53dbf368cf0c0ba5d690af107970358c6
Moshe Levi <mo6709@gmail.com>
2018-01-07 18:32:42 -0500

    update AccountSignupForm to deal with both types of account

--- src/containers/account/AccountSignupForm.js
+++ src/containers/account/AccountSignupForm.js
@@ -6,57 +6,59 @@ import { signupAccount } from '../../actions/accountActions'
 
 class AccountSignupForm extends Component {
     constructor(props){
-        super(props)
+        super(props);
 
         this.state = {
             errors: null,
-            credentials: {
-                name: '',
-                email: '',
-                password: '',
-                passwordConfirmation: '',
-                accountType: '',
-                title: ''
-            }     
+            name: '',
+            email: '',
+            password: '',
+            passwordConfirmation: '',
+            accountType: '',
+            title: '',
+            first_name: '',
+            last_name: '',
         }
     }
     
     handelSignupSubmit = (event) => { 
         event.preventDefault();
-        if (this.formValidation(this.state.credentials)){
+        if (this.formValidation()){
             this.setState({errors: false}); 
-            this.props.signupAccount(this.state.credentials, this.props.history)
+            this.props.signupAccount(this.state, this.props.history)
         }else{
             this.setState({errors: true}); 
         }
     }
 
     handelInputChange = (event) => {
-        const field = event.target.name;
-        const credentials = this.state.credentials;
-        credentials[field] = event.target.value;
-        this.setState({ credentials: credentials });
+        const { name, value } = event.target;
+        this.setState({ [name]: value });
     }
 
-    formValidation = (credentials) => {
-       const { password, passwordConfirmation, name, email, accountType } = credentials 
-       if(password !== "" &&
-          passwordConfirmation === password &&
-          name !== "" &&
-          email !== "" &&
-          accountType !==""){
-           return true;
-       }else{
+    formValidation = () => {
+        const { password, passwordConfirmation, name, email, accountType, title, first_name, last_name } = this.state; 
+        if(password !== "" && passwordConfirmation === password && email !== "" && accountType !==""){
+            switch(accountType){
+                case "corporation":
+                    return (name !== "" && title !== "");
+                case "investor":
+                    return (first_name !== "" && last_name !== ""); 
+            }      
+        }else{
            return false;
-       }
+        }
     }
 
     render(){
+        const { accountType, email, password, passwordConfirmation, name, title, first_name, last_name } = this.state; 
         return(
             <div>               
                 <h2>Signup by Email</h2>
+
                 {this.state.errors === true ? <p>Please fill out all the fields currenctly</p> : ""}
                 <p>{this.props.accountErrors ? this.props.accountErrors[0] : ''}</p>
+                
                 <form onSubmit={event => this.handelSignupSubmit(event) } >
                     <div>
                         {"Are you an "}
@@ -64,7 +66,7 @@ class AccountSignupForm extends Component {
                             <input type="radio"
                             name="accountType"
                             value="investor" 
-                            checked={this.state.credentials.accountType === "investor"}
+                            checked={accountType === "investor"}
                             onChange={this.handelInputChange}/>
                             Investor
                         </label>
@@ -73,7 +75,7 @@ class AccountSignupForm extends Component {
                             <input type="radio"
                             name="accountType" 
                             value="corporation" 
-                            checked={this.state.credentials.accountType === "corporation"}
+                            checked={accountType === "corporation"}
                             onChange={this.handelInputChange}/>
                             Corporation
                         </label> 
@@ -83,23 +85,15 @@ class AccountSignupForm extends Component {
                         <input type="email"
                             name="email"
                             placeholder="Enter email"
-                            value={this.state.credentials.email}
+                            value={email}
                             onChange={this.handelInputChange}/>
                     </label><br/>
-                  
-                    <label>Name: 
-                        <input type="text"
-                            name="name"
-                            placeholder="Enter name"
-                            value={this.state.credentials.name}
-                            onChange={this.handelInputChange}/>
-                    </label><br/>  
 
                     <label>Password: 
                         <input type="password"
                             name="password"
                             placeholder="Enter password"
-                            value={this.state.credentials.password}
+                            value={password}
                             onChange={this.handelInputChange}/>
                     </label><br/>
 
@@ -107,18 +101,45 @@ class AccountSignupForm extends Component {
                         <input type="password"
                             name="passwordConfirmation"
                             placeholder="Confirm password"
-                            value={this.state.credentials.passwordConfirmation}
+                            value={passwordConfirmation}
                             onChange={this.handelInputChange}/>
                     </label><br/>
+                    
+                    <div style={{ display: accountType === 'corporation' ? 'block' : 'none' }}> 
+                        <label> Name: 
+                            <input type="text"
+                                name="name"
+                                placeholder="Enter name"
+                                value={name}
+                                onChange={this.handelInputChange}/>
+                        </label><br/> 
 
-                    <label style={{display: this.state.credentials.accountType === 'corporation' ? 'block' : 'none'}}>
-                        Title:      
-                        <input type="text"
-                            name="title"
-                            placeholder="Enter title"
-                            value={this.state.credentials.title}
-                            onChange={this.handelInputChange}/>
-                    </label><br/>
+                        <label> Title:      
+                            <input type="text"
+                                name="title"
+                                placeholder="Enter title"
+                                value={title}
+                                onChange={this.handelInputChange}/>
+                        </label><br/>
+                    </div>
+
+                    <div style={{ display: accountType === 'investor' ? 'block' : 'none' }}>
+                        <label> First Name: 
+                            <input type="text"
+                                name="first_name"
+                                placeholder="Enter first name"
+                                value={first_name}
+                                onChange={this.handelInputChange}/>
+                        </label><br/> 
+
+                        <label> Last Name:      
+                            <input type="text"
+                                name="last_name"
+                                placeholder="Enter last name"
+                                value={last_name}
+                                onChange={this.handelInputChange}/>
+                        </label><br/>
+                    </div>
  
                     <input type="submit" value="Create Account"/>
                 </form>     
